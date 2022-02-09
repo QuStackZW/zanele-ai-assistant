21142d05106e5d87e7af73cc5a2fd4456bd68767
Google Jr <andilembele020@gmail.com>
2022-02-09 04:40:12 +0200

    Functionality::Number selection

--- index.js
+++ index.js
@@ -75,9 +75,9 @@ app.post("/pharmacist", express.json(), (req, res) => {
       "1. Ask a medical or pharmaceutical related question. \n2. Buy over-the-counter medicine or any of our products \n3. Add Drug"
     );
 
-    agent.add(new Suggestion("Ask a medical question"));
-    agent.add(new Suggestion("Buy medicine"));
-    agent.add(new Suggestion("Add Drug"));
+    agent.add(new Suggestion("1"));
+    agent.add(new Suggestion("2"));
+    agent.add(new Suggestion("3"));
   }
 
   function askPharmacyOrMedicalQuestion(agent) {
