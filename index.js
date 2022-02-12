71ee665bb1caa35384a66b6d53463e2f5baabd95
Google Jr <andilembele020@gmail.com>
2022-02-12 11:49:35 +0200

    Add purchases to the database #4

--- index.js
+++ index.js
@@ -202,10 +202,10 @@ app.post("/pharmacist", express.json(), (req, res) => {
     let paymentPhone = agent.parameters.paymentPhone;
 
     agent.add(
-      `Your Name: ${whoIsBuying} \nOrder: ${drugName} \nDelivery Date: ${deliveryDate} \nDelivery Time: ${deliveryTime} \nDelivery Address: ${deliveryAddress} \nDelivery Phone: ${deliveryPhone} \nPayment Method: ${paymentMethod} \nLinked Number: ${paymentPhone}`
+      `Your Name: ${whoIsBuying} \nOrder: ${drugName} \nDelivery Date: ${deliveryDate} \nDelivery Time: ${deliveryTime} \nDelivery Address: ${deliveryAddress} \nDelivery Phone: ${deliveryPhone} \nPayment Method: ${paymentMethod} \n${paymentMethod} Number: ${paymentPhone}`
     );
 
-    agent.add("Confirm transaction details?");
+    agent.add("Confirm Transaction");
     agent.add(new Suggestion("Yes"));
     agent.add(new Suggestion("No"));
   }
@@ -213,14 +213,11 @@ app.post("/pharmacist", express.json(), (req, res) => {
   async function confirmTransaction(agent) {
     //Details of the purchase
     let drugName = agent.parameters.drugName;
-    // let howMany = agent.parameters.howMany;
     let whoIsBuying = agent.parameters.buyerID;
     let deliveryDate = agent.parameters.deliveryDate;
     let deliveryTime = agent.parameters.deliveryTime;
     let deliveryAddress = agent.parameters.deliveryAddress;
     let deliveryPhone = agent.parameters.deliveryPhone;
-    // let deliveryEmail = agent.parameters.deliveryEmail;
-    // let drugPrice = agent.parameters.drugPrice;
     let paymentMethod = agent.parameters.payMethod;
     let paymentPhone = agent.parameters.paymentPhone;
 
