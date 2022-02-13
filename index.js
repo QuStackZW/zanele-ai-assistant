25299125214841c886e8c1cb01c8e9db2e7cc5d0
Google Jr <andilembele020@gmail.com>
2022-02-13 22:46:42 +0200

    Async function for search #8

--- index.js
+++ index.js
@@ -409,14 +409,28 @@ app.post("/pharmacist", express.json(), (req, res) => {
   // ******************************************** END OF USER ACCOUNT DETAILS***********************************************//
 
   // *********************************************FETCH DRUGS FROM DB*******************************************************//
+  // async function getDrug(agent) {
+  //   const drugRef = db.collection("drugs").doc("Bioplus");
+  //   const doc = await drugRef.get();
+  //   const drugs = [];
+  //   doc.forEach((drug) => {
+  //     drugs.push(drug.data());
+  //   });
+  //   agent.add(`Here are the drugs available: ${drugs}`);
+  // }
+
   async function getDrug(agent) {
-    const drugRef = db.collection("drugs").doc("Bioplus");
+    const drugRef = db.collection("drugs");
     const doc = await drugRef.get();
-    const drugs = [];
-    doc.forEach((drug) => {
-      drugs.push(drug.data());
-    });
-    agent.add(`Here are the drugs available: ${drugs}`);
+    if (!doc.exists) {
+      agent.add("No drug with that name available");
+      console.log("No drug with that name available");
+    } else {
+      agent.add("Here are the drugs available: ");
+      doc.forEach((drug) => {
+        agent.add(`${drug.data().name}`);
+      });
+    }
   }
 
   // *********************************************END OF FETCH DRUGS FROM DB*******************************************************//
@@ -430,7 +444,8 @@ app.post("/pharmacist", express.json(), (req, res) => {
       agent.add("No drugs available.");
     } else {
       snapshot.forEach((doc) => {
-        agent.add(doc.data());
+        agent.add(`${doc.data().name}`);
+        console.log(doc.id, "=>", doc.data());
       });
     }
   }
