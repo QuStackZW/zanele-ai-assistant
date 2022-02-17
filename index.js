167e919caa5712f11fa381a2f0b0288e654be9ad
Google Jr <andilembele020@gmail.com>
2022-02-17 06:05:33 +0200

    UserID

--- index.js
+++ index.js
@@ -186,9 +186,12 @@ app.post("/pharmacist", express.json(), (req, res) => {
 
     //If invalid userId is entered, then return an error message and abort the transaction
     //Compare the userId with the userId in the database
-    let userId = await db.collection("users").doc(userID).get();
+    let userId = await db
+      .collection("users")
+      .where("userID", "==", userID)
+      .get(); //.where("userID", "==", userID)
     if (!userId.exists) {
-      agent.add("Invalid userId. Please try again.");
+      agent.add("Invalid User ID. Please try again.");
       return;
     } else {
       //proceed with the transaction with the userId
